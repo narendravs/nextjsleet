5dbc0e9f7a9cc4af97cd9be63aa1d23d62041e47
narendravs <narendravs_mca@yahoo.com>
2026-02-14 10:49:09 +0530

    refactor(ProblemPage): clean up legacy SSG code and simplify data fetching

--- src/app/problems/[pid]/page.tsx
+++ src/app/problems/[pid]/page.tsx
@@ -3,13 +3,6 @@ import Topbar from "@/components/Topbar/Topbar";
 import Workspace from "@/components/Workspace/Workspace";
 import useHasMounted from "@/hooks/useHasMounted";
 import { problems } from "@/utils/problems";
-import { Problem } from "@/utils/types/problem";
-import React, { useState } from "react";
-//import { data } from "@/utils/problems/data";
-
-// type ProblemPageProps = {
-//   problem: Problem;
-// };
 
 const ProblemPage = ({ params }: { params: { pid: string } }) => {
   const hasMounted = useHasMounted();
@@ -30,38 +23,5 @@ const ProblemPage = ({ params }: { params: { pid: string } }) => {
     </div>
   );
 };
-
-// fetch the local data
-//  SSG
-// getStaticPaths => it create the dynamic routes
-// export async function getStaticPaths() {
-//   const paths = Object.keys(problems).map((key) => ({
-//     params: { pid: key },
-//   }));
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
-
-// // getStaticProps => it fetch the data
-
-// export async function getStaticProps({ params }: { params: { pid: string } }) {
-//   const { pid } = params;
-//   const problem = problems[pid];
-
-//   if (!problem) {
-//     return {
-//       notFound: true,
-//     };
-//   }
-//   problem.handlerFunction = problem.handlerFunction.toString();
-//   return {
-//     props: {
-//       problem,
-//     },
-//   };
-// }
 
 export default ProblemPage;
